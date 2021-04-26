41df8833e3ca9f8746d2d31d169366bd0e426077
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-26 19:56:54 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,33 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The lesson ___ .",
+        options:["has began", "has begun", "have begun", "have began"],
+        answer: 1
+    },
+    {
+        q:"I ___ my mobile phone somewhere but I can't remember where.",
+        options:["have leave", "has left", "have left", "have leaved"],
+        answer: 2
+    },
+    {
+        q:"The teacher ___ yet.",
+        options:["hasn't came", "hasn't come", "haven't come", "haven't came"],
+        answer: 1
+    },
+    {
+     q:"I hope he ___ a mistake.",
+        options:["hasn't made", "haven't made", "hasn't make", "haven't maden"],
+        answer: 0   
+    },
+    {
+        q:"___ he ___ this film yet?",
+        options:["Have / seen", "Has / saw", "Has / seen", "Have / saw"],
+        answer: 2
+    },
+    {
+        q:"___ I ___ you before?",
+        options:["Has / meet", "Has / met", "Have / meet", "Have / met"],
+        answer: 3
+    }
+]
